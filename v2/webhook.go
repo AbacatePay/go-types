aaf181e707b569dde21513a925b12b09fe0c6ec1
almeida <hello@eualmeida.com.br>
2026-01-19 10:21:47 -0300

    feat(webhook): add v2 schemas

--- v2/webhook.go
+++ v2/webhook.go
@@ -24,3 +24,118 @@ func VerifyWebhookSignature(rawBody string, signature string) bool {
 
 	return subtle.ConstantTimeCompare(expectedSig, receivedSig) == 1
 }
+
+// https://docs.abacatepay.com/pages/webhooks
+type WebhookEventType string
+
+// https://docs.abacatepay.com/pages/webhooks
+const (
+	WebhookEventPayoutFailed WebhookEventType = "payout.failed"
+	WebhookEventPayoutDone   WebhookEventType = "payout.done"
+	WebhookEventBillingPaid  WebhookEventType = "billing.paid"
+)
+
+type BaseWebhookEvent[Paylod any] struct {
+	// Unique identifier for the webhook.
+	ID string `json:"id"`
+
+	// Event type.
+	Event WebhookEventType `json:"event"`
+
+	// Event data.
+	Data Paylod `json:"data"`
+
+	// Indicates whether the event occurred in dev mode.
+	DevMode bool `json:"devMode"`
+}
+
+// https://docs.abacatepay.com/pages/webhooks#payout-failed
+type WebhookPayoutFailedEvent BaseWebhookEvent[WebhookPayoutFailedEventData]
+
+// https://docs.abacatepay.com/pages/webhooks#payout-failed
+type WebhookPayoutFailedEventData struct {
+	// Status is always CANCELLED.
+	Transaction APIPayout `json:"transaction"`
+}
+
+// https://docs.abacatepay.com/pages/webhooks#payout-done
+type WebhookPayoutDoneEventData struct {
+	Transaction APIPayout `json:"transaction"`
+}
+
+// https://docs.abacatepay.com/pages/webhooks#payout-done
+type WebhookPayoutDoneEvent BaseWebhookEvent[WebhookPayoutDoneEventData]
+
+type WebhookBillingPaidPayment struct {
+	Fee    uint64        `json:"fee"`
+	Method PaymentMethod `json:"method"`
+	Amount uint64        `json:"amount"`
+}
+
+type WebhookBillingPaidPix struct {
+	// Charge amount in cents (e.g. 4000 = R$40.00).
+	Amount uint64 `json:"amount"`
+
+	// Unique billing identifier.
+	ID string `json:"id"`
+
+	// Always PIX
+	Kind PaymentMethod `json:"kind"`
+
+	// Billing status, can only be `PAID` here.
+	Status PaymentStatus `json:"status"`
+}
+
+type WebhookBillingPaidBilling struct {
+	// Charge amount in cents (e.g. 4000 = R$40.00).
+	Amount int `json:"amount"`
+
+	// Unique billing identifier.
+	ID string `json:"id"`
+
+	// Bill ID in your system.
+	ExternalID string `json:"externalId"`
+
+	// Status of the payment. Always `PaymentStatusPaid`.
+	Status PaymentStatus `json:"status"`
+
+	// URL where the user can complete the payment.
+	URL string `json:"url"`
+}
+
+// https://docs.abacatepay.com/pages/webhooks#billing-paid
+type WebhookBillingPaidEventData struct {
+	Payment WebhookBillingPaidPayment `json:"payment"`
+
+	// Exactly one of these will be non-nil
+
+	PixQrCode *WebhookBillingPaidPix     `json:"pixQrCode,omitempty"`
+	Billing   *WebhookBillingPaidBilling `json:"billing,omitempty"`
+}
+
+// // https://docs.abacatepay.com/pages/webhooks#billing-paid
+type WebhookBillingPaidEvent BaseWebhookEvent[WebhookBillingPaidEventData]
+
+type WebhookEnvelope struct {
+	Event WebhookEventType `json:"event"`
+}
+
+func (event WebhookEnvelope) IsPayoutFailed() bool {
+	return event.Event == WebhookEventPayoutFailed
+}
+
+func (event WebhookEnvelope) IsPayoutDone() bool {
+	return event.Event == WebhookEventPayoutDone
+}
+
+func (event WebhookEnvelope) IsBillingPaid() bool {
+	return event.Event == WebhookEventBillingPaid
+}
+
+func (event WebhookBillingPaidEvent) IsPix() bool {
+	return event.Data.PixQrCode != nil
+}
+
+func (event WebhookBillingPaidEvent) IsBilling() bool {
+	return event.Data.Billing != nil
+}
