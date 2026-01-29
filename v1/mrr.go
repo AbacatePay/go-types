b75126fa571bad71de74d09a80e8ff4734c23279
almeida <hello@eualmeida.com.br>
2026-01-29 15:04:45 -0300

    feat(APIMRRInfo): add missing struct in v1/mrr

--- v1/mrr.go
+++ v1/mrr.go
@@ -6,3 +6,9 @@ type APIMerchantInfo struct {
 	Website   *string `json:"website"`
 	CreatedAt string  `json:"createdAt"`
 }
+
+// https://docs.abacatepay.com/pages/trustMRR/reference#mrr-monthly-recurring-revenue
+type APIMRRInfo struct {
+	MRR                      uint64 `json:"mrr"`
+	TotalActiveSubscriptions uint64 `json:"totalActiveSubscriptions"`
+}
